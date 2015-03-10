3db1de1ce5bfb14980befb8abaa3d270a5a24fbb
Adhil Azeez NV <adhilazeeznv@gmail.com>
2015-03-10 23:37:25 +0530

    Remove unwanted packages

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,6 +1,5 @@
 var changed    = require('gulp-changed');
 var gulp       = require('gulp');
-var imagemin   = require('gulp-imagemin');
 var config     = require('../config').images;
 var browserSync  = require('browser-sync');
 
